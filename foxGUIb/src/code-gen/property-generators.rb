1a0f76b8924f2f22b9d8a6aec4cb0252673ada2f
Lars Kanis <kanis@comcard.de>
2024-07-01 11:32:48 +0200

    standardrb

--- foxGUIb/src/code-gen/property-generators.rb
+++ foxGUIb/src/code-gen/property-generators.rb
@@ -1,58 +1,65 @@
 # Copyright (c) 2004-2006 by Henon (meinrad dot recheis at gmail dot com)
 
 module StringPropCodeGen
-	def to lang, treeitem
-		if lang=="ruby"
-			return "w_#{treeitem}.#{method}#{serialize @wdg.send( method.chop).inspect}"
-		end
-	end
+  def to lang, treeitem
+    if lang == "ruby"
+      "w_#{treeitem}.#{method}#{serialize @wdg.send(method.chop).inspect}"
+    end
+  end
 end
+
 module IntPropCodeGen
-	def to lang, treeitem
-		if lang=="ruby"
-			return "w_#{treeitem}.#{method}#{serialize @wdg.send( method.chop)}"
-		end
-	end
+  def to lang, treeitem
+    if lang == "ruby"
+      "w_#{treeitem}.#{method}#{serialize @wdg.send(method.chop)}"
+    end
+  end
 end
+
 module BoolPropCodeGen
-	def to lang, treeitem
-		if lang=="ruby"
-			return "w_#{treeitem}.#{method}#{serialize @wdg.send( method.chop)}"
-		end
-	end
+  def to lang, treeitem
+    if lang == "ruby"
+      "w_#{treeitem}.#{method}#{serialize @wdg.send(method.chop)}"
+    end
+  end
 end
+
 module ConstPropCodeGen
-	def to lang, treeitem
-		if lang=="ruby"
-			return "w_#{treeitem}.#{method}#{serialize @wdg.send( method.chop)}"
-		end
-	end
+  def to lang, treeitem
+    if lang == "ruby"
+      "w_#{treeitem}.#{method}#{serialize @wdg.send(method.chop)}"
+    end
+  end
 end
+
 module ColorPropCodeGen
-	def to lang, treeitem
-		if lang=="ruby"
-			return "w_#{treeitem}.#{method}Fox::FXRGBA(#{serialize @wdg.send( method.chop)})"
-		end
-	end
+  def to lang, treeitem
+    if lang == "ruby"
+      "w_#{treeitem}.#{method}Fox::FXRGBA(#{serialize @wdg.send(method.chop)})"
+    end
+  end
 end
+
 module FontPropCodeGen
-	def to lang, treeitem
-		if lang=="ruby"
-			return "w_#{treeitem}.#{method}FX::Font.new.from_s('#{serialize( @wdg.send( method.chop))}').to_FXFont"
-		end
-	end
+  def to lang, treeitem
+    if lang == "ruby"
+      "w_#{treeitem}.#{method}FX::Font.new.from_s('#{serialize(@wdg.send(method.chop))}').to_FXFont"
+    end
+  end
 end
+
 module IconPropCodeGen
-	def to lang, treeitem
-		if lang=="ruby"
-			return "w_#{treeitem}.#{method}'#{serialize @wdg.send( method.chop)}'"
-		end
-	end
+  def to lang, treeitem
+    if lang == "ruby"
+      "w_#{treeitem}.#{method}'#{serialize @wdg.send(method.chop)}'"
+    end
+  end
 end
+
 module RangePropCodeGen
-	def to lang, treeitem
-		if lang=="ruby"
-			return "w_#{treeitem}.#{method}#{serialize @wdg.send( method.chop)}"
-		end
-	end
+  def to lang, treeitem
+    if lang == "ruby"
+      "w_#{treeitem}.#{method}#{serialize @wdg.send(method.chop)}"
+    end
+  end
 end
